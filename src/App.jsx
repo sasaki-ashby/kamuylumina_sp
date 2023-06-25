78d9e233805aa363686768d58789358cbe17d011
omiki235 <canomiki05@gmail.com>
2023-06-25 19:39:19 +0900

    ヘッダーのマークアップ対応

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Top from "./pages/top";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Top />
+    </div>
+  );
 }
 
 export default App;
