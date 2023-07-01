4600b87c56ceba29527c2a7a0a027126d7b95697
omiki235 <canomiki05@gmail.com>
2023-07-01 20:01:54 +0900

    fix: View more Button

--- js/spot_more.js
+++ js/spot_more.js
@@ -1,10 +1,5 @@
 document.getElementById("moreButton").addEventListener("click", function () {
-  var hiddenText = document.getElementById("hiddenContainer");
-  if (hiddenText.classList.contains("hidden")) {
-    hiddenText.classList.remove("hidden");
-    document.getElementById("moreButton");
-  } else {
-    hiddenText.classList.add("hidden");
-    document.getElementById("moreButton");
-  }
+  var moreContainer = document.getElementById("moreContainer");
+  moreContainer.classList.remove("hidden");
+  moreContainer.classList.add("centered");
 });
